c34f50620bb01645c5cd9432710cbfce1922090a
Tristan Tan Jing Xuan <jtan0375@student.monash.edu>
2025-05-20 14:32:11 +0800

    Eelim

--- main.py
+++ main.py
@@ -112,18 +112,30 @@ def plot(time_pressure,pressure_list, time_band,band_list,figname,fig_num = 1) :
     plt.subplot(2,1,1)
     plt.plot(time_pressure,pressure_list,color='blue')
     plt.title(f"Pressure Data ({figname})")
-    plt.xlabel("Pressure")
-    plt.ylabel("Time")
-    plt.xlim([0,10])
+    plt.xlabel("Time")
+    plt.ylabel("Pressure")
+    plt.xlim([0,TIME])
     plt.grid()
     plt.subplot(2,1,2)
     plt.plot(time_band,band_list,color='red')
     plt.title(f"Band Data ({figname})")    
-    plt.xlabel("Band")
-    plt.ylabel("Time")
-    plt.xlim([0,10])
+    plt.xlabel("Time")
+    plt.ylabel("Band")
+    plt.xlim([0,TIME])
     plt.grid()
     plt.subplots_adjust(hspace=0.5)  # Increase vertical space between subplots
+    
+def plot_together(time_pressure,pressure_list, time_band,band_list,figname,fig_num = 1) : 
+    fig ,ax1 = plt.subplots(figsize=(10,6))
+    ax2 = ax1.twinx()
+    ax1.plot(time_pressure,pressure_list,color='blue')
+    plt.title(f"Pressure Data/Band Data ({figname})")
+    ax1.set_ylabel("Pressure")
+    ax1.set_xlabel("Time")
+    plt.xlim([0,TIME])
+    ax2.plot(time_band,band_list,color='red') 
+    ax2.set_ylabel("Band")        
+    plt.grid()
       
 def moving_avg_filter(data_list) : 
     moving_avg_data_list = []
@@ -136,9 +148,9 @@ def moving_avg_filter(data_list) :
     
     return moving_avg_data_list
 
-def pred(data_list): 
+def pred(data_list,thres): 
     
-    valley_indices, ___ = find_peaks(data_list, distance=50, prominence=0.3)
+    valley_indices, ___ = find_peaks(data_list, distance=50, prominence=thres)
     
     return len(valley_indices), valley_indices
 
@@ -155,7 +167,7 @@ def main():
             # plotting 
             time_pressure = np.linspace(0,TIME , len(pressure_list) )
             time_band= np.linspace(0,TIME , len(band_list) )
-            plot(time_pressure, pressure_list, time_band, band_list, figname = "Raw",fig_num=1)
+            # plot(time_pressure, pressure_list, time_band, band_list, figname = "Raw",fig_num=1)
             
             # Noise Reduction digiting signal processing 
             mavg_pressure_list = moving_avg_filter(pressure_list)
@@ -163,7 +175,7 @@ def main():
             mavg_band_list = moving_avg_filter(mavg_band_list)
             time_pressure = np.linspace(0,TIME , len(mavg_pressure_list) )
             time_band= np.linspace(0,TIME , len(mavg_band_list) )
-            plot(time_pressure, mavg_pressure_list, time_band, mavg_band_list,figname = "MAVG",fig_num=2)
+            # plot(time_pressure, mavg_pressure_list, time_band, mavg_band_list,figname = "MAVG",fig_num=2)
             
             mean_band = np.mean(mavg_band_list)
             std_band = np.std(mavg_band_list)
@@ -172,15 +184,50 @@ def main():
             mean_pressure = np.mean(mavg_pressure_list)
             std_presure = np.std(mavg_pressure_list)
             mavg_pressure_list_norm = (mavg_pressure_list - mean_pressure) / std_presure
-
             time_pressure = np.linspace(0, TIME, len(mavg_pressure_list_norm))
             time_band = np.linspace(0, TIME, len(mavg_band_list_norm))
 
-            plot(time_pressure, mavg_pressure_list_norm, time_band, mavg_band_list_norm,figname = "MAVG_NORM",fig_num=3)
 
             ## Data Processing     
-            band_pred, ___ = pred(-(np.array(mavg_band_list_norm)))
-            pressure_pred, ___ = pred(-mavg_pressure_list_norm)
+            band_pred, ___ = pred((np.array(mavg_band_list_norm)),0.2)
+            pressure_pred, ___ = pred(-mavg_pressure_list_norm,0.3)
+            
+            print(f"Pressure Pred: {pressure_pred} \nBand pred: {band_pred}")
+            plot(time_pressure, mavg_pressure_list_norm, time_band, mavg_band_list_norm,figname = "MAVG_NORM",fig_num=3)          
+            
+            plot_together(time_pressure, -mavg_pressure_list_norm, time_band, mavg_band_list_norm,figname = "MAVG_NORM_TGT",fig_num=4)
+            
+            combined_data = []
+
+            
+            if len(mavg_band_list_norm) > len(mavg_pressure_list_norm) : 
+                
+                for idx, i in enumerate(mavg_pressure_list_norm) : 
+                    
+                    combined_data.append((-i + mavg_band_list_norm[idx])/2)
+                    combined_time = time_pressure
+                    
+            else : 
+                 
+                 for idx, i in enumerate(mavg_band_list_norm) : 
+                    
+                    combined_data.append((i - (mavg_pressure_list_norm[idx]))/2)
+                    combined_time = time_band
+                    
+            plt.figure(5)
+            plt.plot(combined_time,combined_data)
+            plt.xlabel("Time")
+            plt.ylabel("Data")
+            plt.grid()
+            plt.title("Combined Data Plot")
+            
+                    
+            
+            
+            
+            
+            
+            plt.show()
             
             print(f"Pressure Pred: {pressure_pred} \nBand pred: {band_pred}")
             
